ee8a73e9893a4d67ff9e2d0d75aa5c49c7c691f8
Dave England <DavidEngland@hotmail.com>
2016-10-01 10:27:56 -0500

    Update prettifyDate.js

--- src/helpers/prettifyDate.js
+++ src/helpers/prettifyDate.js
@@ -1,20 +1,20 @@
 module.exports = function(timestamp) {
-  var months = new Array();
-months[0] = "Jan";
-months[1] = "Feb";
-months[2] = "Mar";
-months[3] = "Apr";
-months[4] = "May";
-months[5] = "Jun";
-months[6] = "Jul";
-months[7] = "Aug";
-months[8] = "Sep";
-months[9] = "Oct";
-months[10] = "Nov";
-months[11] = "Dec";
+    var months = new Array();
+    months[0] = "Jan";
+    months[1] = "Feb";
+    months[2] = "Mar";
+    months[3] = "Apr";
+    months[4] = "May";
+    months[5] = "Jun";
+    months[6] = "Jul";
+    months[7] = "Aug";
+    months[8] = "Sep";
+    months[9] = "Oct";
+    months[10] = "Nov";
+    months[11] = "Dec";
 
-  var month = new Date(timestamp).getMonth();
-  var year = new Date(timestamp).getFullYear();
+    var month = new Date(timestamp).getMonth();
+    var year = new Date(timestamp).getFullYear();
 
-  return months[month] + ' ' + year;
+    return months[month] + ' ' + year;
 }
